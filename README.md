46f32bd6b5cb457ef0d27e1a669df433e124c065
BlackBold <toantran014@gmail.com>
2022-07-30 17:37:03 +0700

    JS oop completed

--- README.md
+++ README.md
@@ -25,4 +25,6 @@ Table of contents
 
 ### Abstract classes
 
+Abstract class is a class which can't be instantiated and require subclasses which inherit from a particular abstract class to provice implementations. We will change the animal class to an abstract class. It will not be possilbe to create a instance of this class anymore like on slide number 3 and we will mark makeSound as an abstract method - in order to use it, a subclass must declare its own implementation of this method.
+
 References: @pyplance_code
--- main.js
+++ main.js
@@ -3,6 +3,14 @@ class Animal {
   constructor(name, age) {
     this.name = name;
     this.age = age;
+    if (this.constructor == Animal) {
+      throw new Error("Cannot create a instance of Abstract class");
+    }
+  }
+
+  makeSound() {
+    // return `Some nice sound made`;
+    throw new Error("Abstract method doesn't have an implementation");
   }
 
   getInfor() {
@@ -11,11 +19,63 @@ class Animal {
 }
 
 // First instance
-const firstAnimal = new Animal("Nana", 3);
-console.log(firstAnimal.getInfor());
+// const firstAnimal = new Animal("Nana", 3);
+// console.log(firstAnimal.getInfor());
 
-const secondAnimal = new Animal("Lulu", 10);
-console.log(secondAnimal.getInfor());
+// const secondAnimal = new Animal("Lulu", 10);
+// console.log(secondAnimal.getInfor());
 
 // Inheritance
-class Dog extends Animal {}
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
+
+  makeSound() {
+    return "woof";
+  }
+  // bark() {
+  //   return "woof";
+  // }
+}
+
+class Cat extends Animal {
+  #weight;
+  constructor(name, age, weight) {
+    super(name, age);
+    this.#weight = weight;
+  }
+
+  // makeSound() {
+  //   return "meow";
+  // }
+
+  // ENCAPSULATION //
+  getWeight() {
+    // getter
+    return this.#weight;
+  }
+
+  setWeight(weight) {
+    // setter
+    this.#weight = weight;
+  }
+}
+
+// const myDog = new Dog("Nana", 1, "Grass dog");
+// console.log(myDog.getInfor());
+// console.log(myDog.breed);
+// console.log(myDog.bark());
+
+// POLYMORPHISM //
+
+const myCat = new Cat("Meow", 5, "4kg");
+const myDog = new Dog("Nana", 1, "Grass dog");
+console.log(myCat.makeSound());
+console.log(myDog.makeSound());
+
+// const myCat = new Cat("Meow", 5, "4kg");
+// console.log(myCat.getWeight());
+// myCat.setWeight("4.5kg");
+// console.log(myCat.getWeight());
